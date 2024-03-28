8a36c443658abe43b403237c9d66bf8644615dec
Thaís Tardy <134325248+devthaistardy@users.noreply.github.com>
2024-03-27 21:21:35 -0300

    Add arquivos
    Adicionando os arquivos do projeto

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const imagensPainel = document.querySelectorAll('.imagem-painel');
+const setaAvancar = document.getElementById('btn-avancar');
+const setaVoltar = document.getElementById('btn-voltar');
+let imagemAtual = 0;
+
+setaAvancar.addEventListener('click', function() {
+  
+})
